8a540d08d159205aad3ab0f61c2658b408f1e4ab
Mark Slater <mws@localhost.localdomain>
2021-11-02 11:43:32 +0000

    Remove the render timer

--- samba/lect.c
+++ samba/lect.c
@@ -7991,8 +7991,7 @@ relance:
 		pthread_t lectexec_thread;
 		int t = pthread_create(&lectexec_thread, NULL, LectExecThread, 
 			(void *) entretien_bolo,
-			(void *) synchroD2traitees);
-		OpiumStartRenderTimer();
+			(void *) syncehroD2traitees);
 		return;
 #else
 		LectExecThread();
